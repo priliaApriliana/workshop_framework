cfb0f03f989f906291d2a262fa681740a9876efc
Putri Apriliana <pu3apriliana@gmail.com>
2026-02-19 15:07:46 +0700

    upload new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,10 +27,6 @@
             background-color: #f5f5f5; 
             border-color: #ccc;
         }
-        .btn-google img {
-            width: 20px;
-            height: 20px;
-        }
         .divider {
             display: flex;
             align-items: center;
@@ -66,7 +62,7 @@
 
                             {{-- Alert Messages --}}
                             @if (session('success'))
-                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
                                     {{ session('success') }}
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
@@ -75,32 +71,16 @@
                             @endif
 
                             @if (session('error'))
-                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
                                     {{ session('error') }}
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
                             @endif
-
-                            {{-- Google Login Button --}}
-                            <div class="pt-3">
-                                <a href="{{ route('auth.google') }}" class="btn btn-block btn-lg btn-google">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
-                                        <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
-                                        <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
-                                        <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/>
-                                        <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
-                                    </svg>
-                                    Sign in with Google
-                                </a>
-                            </div>
-
-                            <div class="divider">
-                                <span>OR</span>
-                            </div>
                             
-                            <form method="POST" action="{{ route('login') }}">
+                            {{-- Form Login --}}
+                            <form class="pt-3" method="POST" action="{{ route('login') }}">
                                 @csrf
                                 <div class="form-group">
                                     <input type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" 
@@ -129,12 +109,31 @@
                                         <a href="{{ route('password.request') }}" class="auth-link text-black">Forgot password?</a>
                                     @endif
                                 </div>
-                                @if (Route::has('register'))
-                                <div class="text-center mt-4 font-weight-light">
-                                    Don't have an account? <a href="{{ route('register') }}" class="text-primary">Create</a>
-                                </div>
-                                @endif
                             </form>
+
+                            {{-- Divider --}}
+                            <div class="divider">
+                                <span>OR</span>
+                            </div>
+
+                            {{-- Google Login Button - DI BAWAH SIGN IN --}}
+                            <div>
+                                <a href="{{ route('auth.google') }}" class="btn btn-block btn-lg btn-google">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+                                        <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
+                                        <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
+                                        <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/>
+                                        <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
+                                    </svg>
+                                    Sign in with Google
+                                </a>
+                            </div>
+
+                            @if (Route::has('register'))
+                            <div class="text-center mt-4 font-weight-light">
+                                Don't have an account? <a href="{{ route('register') }}" class="text-primary">Create</a>
+                            </div>
+                            @endif
                         </div>
                     </div>
                 </div>
